3f1dff014184c705965ada43900f773a23ca4fbb
Tongshak <sabotongshak225@gmail.com>
2025-07-16 16:56:37 +0000

    add class

---
+++ task.py
@@ -0,0 +1,26 @@
+# A simple calculator
+
+allowance = 2000
+
+allowance -= 400
+print(f"remain: {allowance}")
+
+allowance  += 100
+print(f"add: {allowance}")
+
+allowance -=250
+print(f"bough snacks: {allowance}")
+
+allowance *=0.75
+print(f"gave 25% out: {allowance}")
+
+allowance = ((2/3) * allowance)
+print(f"used one-third: {allowance}")
+
+allowance = ((allowance) //2)
+print(f"split: {allowance}")
+
+allowance %= 100
+print(f"final: {allowance}")
+
+
